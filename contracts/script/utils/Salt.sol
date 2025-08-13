5cf67f3c1729df24cb4194c56e11aae4d5dfb8e9
Preston Ong <preston4896@me.com>
2025-08-13 18:21:52 +0800

    update tpm

--- contracts/script/utils/Salt.sol
+++ contracts/script/utils/Salt.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.8.0;
 
 bytes32 constant WORKLOAD_VERIFIER_PROXY_SALT = keccak256(bytes("WORKLOAD_VERIFIER_PROXY_SALT"));
 bytes32 constant WORKLOAD_VERIFIER_IMPL_SALT = keccak256(bytes("WORKLOAD_VERIFIER_IMPL_SALT"));
-bytes32 constant CVM_VERIFIER_PROXY_SALT = keccak256(bytes("CVM_VERIFIER_PROXY_SALT"));
-bytes32 constant CVM_VERIFIER_IMPL_SALT = keccak256(bytes("CVM_VERIFIER_IMPL_SALT"));
+bytes32 constant CVM_REGISTRY_PROXY_SALT = keccak256(bytes("CVM_REGISTRY_PROXY_SALT"));
+bytes32 constant CVM_REGISTRY_IMPL_SALT = keccak256(bytes("CVM_REGISTRY_IMPL_SALT"));
