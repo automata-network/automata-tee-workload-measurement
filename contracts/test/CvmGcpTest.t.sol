b08646b600b47ab83141bf6bf14e9180dee5658c
Preston Ong <preston4896@me.com>
2025-12-18 17:57:24 +0800

    updated tests

--- contracts/test/CvmGcpTest.t.sol
+++ contracts/test/CvmGcpTest.t.sol
@@ -8,7 +8,7 @@ import {Base64} from "solady/utils/Base64.sol";
 import {Measurement} from "../src/lib/LibTEE.sol";
 
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {CVMRegistry} from "../src/usecases/CVMRegistry.sol";
+import {CVMRegistry, CVMConfig} from "../src/usecases/CVMRegistry.sol";
 
 contract CvmGcpTest is TestSetup {
     CVMRegistry registry;
@@ -27,8 +27,8 @@ contract CvmGcpTest is TestSetup {
     }
 
     function testGcpTdxCvm() public {
-        // pinned December 18th, 2025, 0700h UTC
-        vm.warp(1766041200);
+        // pinned December 18th, 2025, 0915h UTC
+        vm.warp(1766049300);
         bytes memory googleCa = vm.readFileBinary(string.concat(vm.projectRoot(), "/test/testdata/gcp/tpmAkRoot.der"));
         vm.prank(owner);
         tpmAttestation.addCA(googleCa);
@@ -59,11 +59,29 @@ contract CvmGcpTest is TestSetup {
 
         (bool reattestSuccess, bytes memory reattestResult) = address(registry).call(decodedReattestCalldata);
         assertTrue(reattestSuccess, string(reattestResult));
+
+        bytes32 cvnIdentityHash = 0x17c7bb7b794e50177bc50f46a53f6ee57e4ca4377b7276b208b39c915988d80b;
+        CVMConfig memory config = registry.getCvmConfig(cvnIdentityHash);
+
+        uint64 teeTTLBefore = config.teeTTL;
+        uint64 tpmTTLBefore = config.tpmTTL;
+
+        string memory ttlUpdateJson = 
+            vm.readFile(string.concat(vm.projectRoot(), "/test/testdata/gcp/tdx/gcp-tdx-update-ttl.json"));
+        string memory encodedTtlUpdateCalldata = ttlUpdateJson.readString(".calldata");
+        bytes memory decodedTtlUpdateCalldata = Base64.decode(encodedTtlUpdateCalldata);
+
+        (bool ttlUpdateSuccess, ) = address(registry).call(decodedTtlUpdateCalldata);
+        assertTrue(ttlUpdateSuccess, "TTL update failed");
+
+        CVMConfig memory updatedConfig = registry.getCvmConfig(cvnIdentityHash);
+        assertNotEq(updatedConfig.teeTTL, teeTTLBefore, "TEE TTL not updated");
+        assertNotEq(updatedConfig.tpmTTL, tpmTTLBefore, "TPM TTL not updated");
     }
 
     function testGcpSevSnp() public {
-        // pinned September 5th, 2025, 0200h UTC
-        vm.warp(1757037600);
+        // pinned December 18th, 2025, 0900h UTC
+        vm.warp(1766048400);
         bytes memory googleCa = vm.readFileBinary(string.concat(vm.projectRoot(), "/test/testdata/gcp/tpmAkRoot.der"));
         vm.prank(owner);
         tpmAttestation.addCA(googleCa);
@@ -94,5 +112,23 @@ contract CvmGcpTest is TestSetup {
 
         (bool reattestSuccess, bytes memory reattestResult) = address(registry).call(decodedReattestCalldata);
         assertTrue(reattestSuccess, string(reattestResult));
+
+        bytes32 cvnIdentityHash = 0x1fe9147ef3a853e1412adcd1d01ed876f5aac76ffae7417a87c28da4870202f0;
+        CVMConfig memory config = registry.getCvmConfig(cvnIdentityHash);
+
+        uint64 teeTTLBefore = config.teeTTL;
+        uint64 tpmTTLBefore = config.tpmTTL;
+
+        string memory ttlUpdateJson = 
+            vm.readFile(string.concat(vm.projectRoot(), "/test/testdata/gcp/snp/gcp-snp-update-ttl.json"));
+        string memory encodedTtlUpdateCalldata = ttlUpdateJson.readString(".calldata");
+        bytes memory decodedTtlUpdateCalldata = Base64.decode(encodedTtlUpdateCalldata);
+
+        (bool ttlUpdateSuccess, ) = address(registry).call(decodedTtlUpdateCalldata);
+        assertTrue(ttlUpdateSuccess, "TTL update failed");
+
+        CVMConfig memory updatedConfig = registry.getCvmConfig(cvnIdentityHash);
+        assertNotEq(updatedConfig.teeTTL, teeTTLBefore, "TEE TTL not updated");
+        assertNotEq(updatedConfig.tpmTTL, tpmTTLBefore, "TPM TTL not updated");
     }
 }
